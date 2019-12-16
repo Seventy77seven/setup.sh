9486e27a08398b9c0f5dc6f51f9af8eb214d57a4
Michael Rudnick <32645758+Seventy77seven@users.noreply.github.com>
2019-12-16 16:21:02 -0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,117 @@
+    
+# Ubuntu Updates
+sudo apt update
+sudo apt upgrade
+# GENERAL LINUX
+sudo apt-get update  # updates the package index cache
+sudo apt-get upgrade -y  # updates packages
+sudo apt-get install -y git screen htop wget nano bzip2 # installs system tools
+sudo apt-get upgrade -y bash  # upgrades bash if necessary
+sudo apt-get clean #cleans up package cache
+
+sudo apt install python-pip -y # --upgrade pip
+ubuntu-support-status
+
+    sudo apt-get -y install build-essential #essenqUTO_INCREMENT	tial C/C++ Compiler
+    sudo apt-get -y install git # content tracker to store content
+#    sudo apt-get -y install bash-completion # auto complete -hit TAB
+    sudo apt-get -y install wget curl #fetch files
+
+    sudo apt-get -y install python3 python3-dev # dev = C extentions
+    sudo apt-get -y install libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
+    # XML processing libraries
+    sudo apt-get -y install python3-setuptools #package develpment process library
+
+    sudo apt-get -y install gsl-bin libgsl-dev # GNU Scientific Library 4 C wrapper
+    sudo apt-get -y install rdesktop # remote desktop 
+    #sudo apt-get -y install mongodb # open SOurce database
+    
+    sudo apt-get -y install python-pip #package manager
+    sudo apt-get -y install python3-pip #package manager
+    sudo apt-get -y install sqlite3 # python database engine for SQL
+    pip3 install virtualenv # isolated env to solve specific package dependencies
+    pip3 install requests # facilitates http requests
+    pip3 install ipython
+    pip3 install ipython notebook
+    pip3 install xlrd # module to facilitate reading excel files
+    pip3 install openpyxl # again, excel files
+    #pip3 install pymongo #like psycopg2 but for mongodb
+
+    pip3 install django #python web framework
+    sudo apt-get install libpq-dev -y # lib to help install psycopg2
+    pip3 install psycopg2 # python lib for postgres interaction
+    pip3 install networkX #structure, dynamics, and functions of complex networks
+    pip3 install flask #micro web framework written in python
+    pip3 install redis # support abstract datastructures
+
+    	pip3 install numpy # fundamental package for scientific computing
+	pip3 install scipy # builds off of numpy
+	pip3 install sympy # symbolic mathmatics
+	pip3 install matplotlib # plotting library
+    	pip3 install pandas # data analysis
+    	pip3 install bs4 # beautifulSoup web scrapping
+    	pip3 install statsmodels # statistical package
+    	pip3 install scrapy # webcrawler for scraping and extracting API data
+    	pip3 install seaborn # statistical data visualization
+    	pip3 install bokeh # plotting library
+	pip3 install scikit-learn # supervised and unsupervised learning algos
+    
+    	pip3 install pymysql # python MySQL client library
+    	pip3 install sqlalchemy # interface for creating and executing database code
+    	pip3 install spark # processing, querying, analysing BigData
+    	pip3 install tensorflow # Deep learning models
+    	pip3 install lxml # processes html and xml
+
+	pip3 install q # logging and debugging
+	pip3 install cufflinks #Combining plotly with pandas
+	pip3 install plotly # interactive D3.js plots
+	sudo apt install tree	# Provides a visual  tree file structure
+
+# Find the Fastest Repository Mirror
+#   Software & Updates >> Ubuntu Software >> Download from >> Other >> Select Best Server
+
+# WHen should I enable firewall?  Enable == sudo ufw enable >> Disable == sud ufw disable
+
+sudo apt purge ubuntu-web-launchers # Remove Amazon Launcher
+
+# NightLight == Settings>> Devices >> Screen Display >> NightLight
+
+#Conky System Monitoring
+# sudo apt install conky-all
+
+#Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+sudo apt install ubuntu-restricted-extras -y # Third party codec (MP3's etc)
+
+# Drivers??  Firmware??
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
